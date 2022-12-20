1eda234921c7c55fafc878712fca4b86a341a392
ZiGaXOXO <zekasulmin@gmail.com>
2022-12-21 03:05:58 +1000

    Added Struct films

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct films{
+    char film[150];
+    int year;
+    char country[150];
+    char genre[150];
+    float rating;
+};
+
 
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
